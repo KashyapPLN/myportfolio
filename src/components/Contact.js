24535eecbc82819f642e12ef418ad6d0eb003f33
Kashyap PLN <kashyap.pln@gmail.com>
2022-11-12 20:49:48 +0530

    warnings removed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,17 +9,17 @@ function Contact({contactRef}) {
         <Container style={{display:'flex',justifyContent:'space-evenly'}}>
             
           <Container className='phone'>
-            <img src='https://media1.giphy.com/media/mbW2nvTE0TUc5IgRMm/giphy.gif?cid=6c09b95292377b798e2a21062d1c0c31021cc4209e405fd6&rid=giphy.gif&ct=s'style={{width:'80px',height:'80px'}}/>
+            <img src='https://media1.giphy.com/media/mbW2nvTE0TUc5IgRMm/giphy.gif?cid=6c09b95292377b798e2a21062d1c0c31021cc4209e405fd6&rid=giphy.gif&ct=s'style={{width:'80px',height:'80px'}} alt='phone'/>
             <p style={{color:'rgb(248, 245, 207)',fontWeight:'bold'}}>Phone</p>
             <p style={{color:'rgb(248, 245, 207)',fontWeight:'bold'}}>+918121656640</p>
           </Container>  
           <Container className='phone'>
-            <img className='emailicon'src='https://i.ibb.co/2nPyKbn/ezgif-3-6d2330e2b8.gif'style={{width:'70px',height:'70px'}}/>
+            <img className='emailicon'src='https://i.ibb.co/2nPyKbn/ezgif-3-6d2330e2b8.gif'style={{width:'70px',height:'70px'}} alt='emailicon'/>
             <p style={{color:'rgb(248, 245, 207)',fontWeight:'bold'}}>Email address</p>
             <p style={{color:'rgb(248, 245, 207)',fontWeight:'bold'}}>kashyap.pln@gmail.com</p>
           </Container>  
           <Container className='phone'>
-            <img className='emailicon'src='https://i0.wp.com/fiverrpromotion.com/wp-content/uploads/2022/03/63-home-flat.gif?fit=375%2C375&ssl=1'style={{width:'70px',height:'70px'}}/>
+            <img className='emailicon'src='https://i0.wp.com/fiverrpromotion.com/wp-content/uploads/2022/03/63-home-flat.gif?fit=375%2C375&ssl=1'style={{width:'70px',height:'70px'}} alt='homeicon'/>
             <p style={{color:'rgb(248, 245, 207)',fontWeight:'bold'}}>Location</p>
             <p style={{color:'rgb(248, 245, 207)',fontWeight:'bold'}}>Visakhapatnam </p>
           </Container>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import reactboot from '../reactboot.png';
-import { IconButton } from '@mui/material';
+
 
 // import giphy from '../giphy.gif';
 
@@ -21,51 +21,51 @@ function Skills({skillref}) {
           </Container>
             <Container className='skills'>
                 <div >
-                <img className='techskill' src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748__340.png'/>
+                <img className='techskill' src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748__340.png' alt='html5'/>
                 <p className='sname'>HTML 5</p>
                 </div>
                 <div >
-                <img className='techskill'src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png'/>
+                <img className='techskill'src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_960_720.png' alt='css3'/>
                 <p className='sname'>CSS 3</p>
                 </div>
                 <div >
-                <img className='techskill'src='https://cdn-icons-png.flaticon.com/512/5968/5968672.png'/>
+                <img className='techskill'src='https://cdn-icons-png.flaticon.com/512/5968/5968672.png' alt='bootstrap'/>
                 <p className='sname'>Bootstrap</p>
                 </div>
                 <div>
-                <img className='techskill'src='https://www.freepnglogos.com/uploads/javascript-png/js-logo-png-5.png'/>
+                <img className='techskill'src='https://www.freepnglogos.com/uploads/javascript-png/js-logo-png-5.png' alt='js'/>
                 <p className='sname'>JavaScript</p>
                 </div>
                 <div>
-                <img className='techskill'src='https://d216p14sibfuxn.cloudfront.net/images/modules/react.gif'/>
+                <img className='techskill'src='https://d216p14sibfuxn.cloudfront.net/images/modules/react.gif' alt='react'/>
                 <p className='sname'>React</p>
                 </div>
                 <div>
-                <img className='techskill'src={reactboot}/>
+                <img className='techskill'src={reactboot} alt='react-bootstrap'/>
                 <p className='sname'>React Bootstrap</p>
                 </div>
                 <div>
-                <img className='techskill'src='https://v4.material-ui.com/static/logo.png'/>
+                <img className='techskill'src='https://v4.material-ui.com/static/logo.png' alt='mui'/>
                 <p className='sname'>Material UI</p>
                 </div>
                 <div>
-                <img className='techskill'src='https://icon-library.com/images/node-js-icon/node-js-icon-8.jpg'/>
+                <img className='techskill'src='https://icon-library.com/images/node-js-icon/node-js-icon-8.jpg' alt='nodejs'/>
                 <p className='sname'>Node JS</p>
                 </div>
                 <div>
-                <img className='techskill'src='https://camo.githubusercontent.com/4fca8f8b5d77dc3a28ef31a193aeb76683e2fe52e7ad24a75fef048cd6601f1d/68747470733a2f2f7777772e6f72616e67656d616e7472612e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031392f30352f657870726573736a732d69636f6e2e706e67'/>
+                <img className='techskill'src='https://camo.githubusercontent.com/4fca8f8b5d77dc3a28ef31a193aeb76683e2fe52e7ad24a75fef048cd6601f1d/68747470733a2f2f7777772e6f72616e67656d616e7472612e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031392f30352f657870726573736a732d69636f6e2e706e67' alt='express js'/>
                 <p className='sname'>Express JS</p>
                 </div>
                 <div>
-                <img className='techskill'src='https://cdn.iconscout.com/icon/free/png-256/mongodb-5-1175140.png'/>
+                <img className='techskill'src='https://cdn.iconscout.com/icon/free/png-256/mongodb-5-1175140.png' alt='mongo db'/>
                 <p className='sname'>Mongo DB</p>
                 </div>
                 <div>
-                <img className='techskill'src='https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png'/>
+                <img className='techskill'src='https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png' alt='postman'/>
                 <p className='sname'>Postman</p>
                 </div>
                 <div>
-                <img className='techskill'src='https://cdn.iconscout.com/icon/free/png-256/java-60-1174953.png'/>
+                <img className='techskill'src='https://cdn.iconscout.com/icon/free/png-256/java-60-1174953.png'alt='java'/>
                 <p className='sname'>Java</p>
                 </div>
                 {/* https://github.githubassets.com/images/modules/logos_page/Octocat.png */}
